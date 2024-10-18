db7ff9bf7a2472fce15225ca8f0ad443080dd0f9
davidkochanski <kochanskidavid9@gmail.com>
2024-10-18 14:01:08 -0400

    test

--- test.py
+++ test.py
@@ -1,46 +1,10 @@
-import pandas as pd
-from confederations import to_confededration
+import ultralytics
+yo = ultralytics.YOLO("models/best.pt")
 
-data_frame = pd.read_csv("data/results.csv")
+result = yo.predict("data/video.mp4", save=True)
 
-games = data_frame.to_dict(orient="records")
+print(result[0])
 
-teams = {}
-
-for game in games:
-    home = game["home_team"]
-    away = game["away_team"]
-    home_goals = game["home_score"]
-    away_goals = game["away_score"]
-    
-    if pd.isna(home_goals) or pd.isna(away_goals):
-        continue
-    
-    if home not in teams:
-        teams.update({home: {"name": home, "games": 0, "goals_for": 0, "goals_against": 0}})
-        
-    if away not in teams:
-        teams.update({away: {"name": away, "games": 0, "goals_for": 0, "goals_against": 0}})
-    
-    teams[home]["games"] += 1
-    teams[away]["games"] += 1
-    
-    teams[home]["goals_for"] += home_goals
-    teams[away]["goals_for"] += away_goals
-    
-    teams[away]["goals_against"] += home_goals
-    teams[home]["goals_against"] += away_goals
-    
-for team in teams.values():
-    team["confederation"] = to_confededration(team["name"])
-
-    avg_goals = round(team["goals_for"] / team["games"], 2)
-    avg_conceded = round(team["goals_against"] / team["games"], 2)
-    
-    team["average_goals_per_game"] = avg_goals
-    team["average_conceded_per_game"] = avg_conceded
-    
-
-teams_data_frame = pd.DataFrame.from_dict(teams, orient='index')
-
-teams_data_frame.to_csv("data/teams.csv", index=False)
+for box in result[0].boxes:
+    print(box)
+    print("===========")
